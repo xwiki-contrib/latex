a43ddd9e21d3b40792d4fd5495d1f35a3ea894e6
Vincent Massol <vincent@massol.net>
2018-04-12 16:22:22 +0200

    LATEX-53: xcontext.action doesn't return "latexexport"

--- latex-export/src/main/java/org/xwiki/contrib/latex/internal/export/LaTeXExportResourceReferenceHandler.java
+++ latex-export/src/main/java/org/xwiki/contrib/latex/internal/export/LaTeXExportResourceReferenceHandler.java
@@ -23,25 +23,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import javax.inject.Inject;
-/*
- * See the NOTICE file distributed with this work for additional
- * information regarding copyright ownership.
- *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- */
 import javax.inject.Named;
 import javax.inject.Singleton;
 
@@ -53,7 +34,6 @@ import org.xwiki.resource.AbstractResourceReferenceHandler;
 import org.xwiki.resource.ResourceReference;
 import org.xwiki.resource.ResourceReferenceHandlerChain;
 import org.xwiki.resource.ResourceReferenceHandlerException;
-import org.xwiki.resource.annotations.Authenticate;
 import org.xwiki.resource.entity.EntityResourceAction;
 import org.xwiki.resource.entity.EntityResourceReference;
 import org.xwiki.security.authorization.AccessDeniedException;
@@ -68,7 +48,6 @@ import org.xwiki.security.authorization.Right;
 @Component
 @Named(LaTeXExportResourceReferenceHandler.ACTION_STRING)
 @Singleton
-@Authenticate
 public class LaTeXExportResourceReferenceHandler extends AbstractResourceReferenceHandler<EntityResourceAction>
 {
     /**
@@ -132,5 +111,4 @@ public class LaTeXExportResourceReferenceHandler extends AbstractResourceReferen
             }
         }
     }
-
 }
